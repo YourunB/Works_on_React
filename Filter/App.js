dd77f05f52e2400964e095a3883023a05b8b0517
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2023-11-07 21:53:17 +0300

    refactor: delete folder and files

--- Filter/App.js
+++
@@ -1,9 +0,0 @@
-﻿import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Filter from './components/filter.js';
-
-ReactDOM.render( 
-  <Filter
-    arrInit={['california', 'everything', 'aboveboard', 'washington', 'basketball', 'weathering', 'characters', 'literature', 'contraband', 'appreciate']}
-  />, document.getElementById('container') );
