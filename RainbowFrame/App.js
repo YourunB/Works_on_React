e45f64b5668b446e1be309530a6fe6984b4e9942
YourunB <bxyura@gmail.com>
2023-11-07 21:56:49 +0300

    feat: add files for RainbowFrame

---
+++ RainbowFrame/App.js
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RainbowFrame from './components/RainbowFrame.js';
+
+ReactDOM.render( 
+  <RainbowFrame
+    colors = {['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple']}
+    text = {'Hello!'}
+  />, document.getElementById('container') );
