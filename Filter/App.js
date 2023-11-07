2ec12f65c0a9af222f57774c5c79b930ed31d20a
YourunB <bxyura@gmail.com>
2023-11-07 22:12:42 +0300

    feat: add folder and files

---
+++ Filter/App.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Filter from './components/filter.js';
+
+ReactDOM.render( 
+  <Filter
+    arrInit={['california', 'everything', 'aboveboard', 'washington', 'basketball', 'weathering', 'characters', 'literature', 'contraband', 'appreciate']}
+  />, document.getElementById('container') );
