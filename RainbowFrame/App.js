942ccf2fb207d6242a4fb51fd3bfb8a9c6610587
YourunB <bxyura@gmail.com>
2023-11-09 16:23:21 +0300

    fix: add text is perfect as a komonent's child

--- RainbowFrame/App.js
+++ RainbowFrame/App.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import RainbowFrame from './components/RainbowFrame.js';
 
 ReactDOM.render( 
-  <RainbowFrame
-    colors = {['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple']}
-    text = {'Hello!'}
-  />, document.getElementById('container') );
+  <RainbowFrame  colors = {['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple']}>
+    Hello!
+  </RainbowFrame>,document.getElementById('container') );
--- RainbowFrame/components/RainbowFrame.js
+++ RainbowFrame/components/RainbowFrame.js
@@ -6,19 +6,17 @@ import './RainbowFrame.css';
 class RainbowFrame extends React.Component {
 
   static propTypes = {
-    text:PropTypes.string.isRequired,
     colors:PropTypes.array.isRequired,
   };
 
   state = {
-    text: this.props.text,
     colors: this.props.colors,
   };
 
   createFrames = () => {
     let result = [];
     for (let i = 0; i < this.state.colors.length; i++) {
-      if (i === 0) result.push(<div className='frame' key={this.state.colors[i]} style={{borderColor:this.state.colors[i]}}><p>{this.state.text}</p></div>);
+      if (i === 0) result.push(<div className='frame' key={this.state.colors[i]} style={{borderColor:this.state.colors[i]}}><p>{this.props.children}</p></div>);
       if (i > 0 && i < this.state.colors.length) result.push(<div className='frame' key={this.state.colors[i]} style={{borderColor:this.state.colors[i]}}>{result[i-1]}</div>);
     }
     return result[this.state.colors.length - 1];
