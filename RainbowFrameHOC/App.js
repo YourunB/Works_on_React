e48f2a54f2fc3014897e4d7486ce7588caed0469
YourunB <bxyura@gmail.com>
2023-11-15 09:43:53 +0300

    refactor: delete rainbow component

--- RainbowFrameHOC/App.js
+++ RainbowFrameHOC/App.js
@@ -1,7 +1,35 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
 
-import RainbowFrame from './components/RainbowFrame.js';
+import './components/DoubleButton.css';
+
+import DoubleButton from './components/DoubleButton.js';
+import { withRainbowFrame } from './components/withRainbowFrame';
+
+class App extends React.Component {
+
+  state = {
+    btn: 1,
+  }
+
+  changeBtn = (n) => {
+    this.setState( {btn: n} )
+  }
+
+  render() {
+
+    const DoubleButtonWithFrame = withRainbowFrame(['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'])(DoubleButton);
+
+    return (
+      <div>
+        <DoubleButtonWithFrame changeBtn = { this.changeBtn }  caption1 = {(this.state.btn === 1) ? 'Я из лесу' : 'Однажды'} caption2 = {(this.state.btn === 1) ? 'мороз' : 'пору'}> {(this.state.btn === 1) ? ' вышел, был сильный ' : ' студеную зимнюю '}</DoubleButtonWithFrame>
+      </div>
+    );
+  }
+
+}
+
+export default App;
 
 ReactDOM.render( 
-  <RainbowFrame/>, document.getElementById('container') );
+  <App/>, document.getElementById('container') );
