affa271f3c4d75d1813edf6ce8fb8d69377d9c55
YourunB <bxyura@gmail.com>
2023-11-22 11:29:05 +0300

    refactor: redesigned into 3 components

---
+++ FilterHook/components/FilterControls.js
@@ -0,0 +1,25 @@
+﻿import React, { useState, useEffect } from 'react';
+
+export default props => {
+
+  const [checked, setChecked] = useState(false);
+  const [find, setFind] = useState('');
+
+  function changeSort() { (checked === false) ? setChecked(true) : setChecked(false);  }
+  function changeFilter() { setFind(event.target.value); }
+
+  function reset() {
+    setChecked(false);
+    setFind('');
+  }
+
+  useEffect( () => { props.workCode(find, checked); }, [checked, find] );
+
+  return (
+    <header>
+      <input type='checkbox' checked={checked} onChange={changeSort} id='checkbox-sort'></input><label htmlFor='checkbox-sort'></label><span>&#8693;</span>
+      <input value={find} onChange={changeFilter} type="text"/>
+      <button onClick={reset}>Reset</button>
+    </header>
+  );
+};
---
+++ FilterHook/components/FilterResult.js
@@ -0,0 +1,10 @@
+﻿import React, { } from 'react';
+
+export default props => {
+
+  return (
+    <main>
+      <textarea value={props.arr.join('\n')} disabled rows={10}></textarea>
+    </main>
+  );
+};
